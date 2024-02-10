0856c27e8304398b27cffd72f9c837989aecbd10
James Mohr <jmohr946@gmail.comgit cd..>
2024-02-10 07:34:38 -0500

    SimpleCalculator LWC to James

---
+++ force-app/main/default/lwc/simpleCalculator/simpleCalculator.js
@@ -0,0 +1,57 @@
+import { LightningElement, track } from 'lwc';
+
+export default class SimpleCalculator extends LightningElement {
+    @track currentResult;
+    @track previousResults =[];
+    @track showPreviousResults = false;
+
+    firstNumber;
+    secondNumber;
+
+    numberChangeHandler(event){
+        const inputBox = event.target.name;
+        if(inputBox === 'firstNumber'){
+            this.firstNumber = event.target.value;
+        }else if(inputBox === 'secondNumber'){
+            this.secondNumber = event.target.value;
+    }
+
+}
+
+addHandler(){
+    const firstN = parseInt(this.firstNumber);
+    const secondN = parseInt(this.secondNumber);
+
+    this.currentResult = 'Result of '+firstN+'+'+secondN+' is ' +(firstN+secondN);
+    //this.currentResult = 'Result of ${firstN}+${secondN} is ${firstN+secondN}';
+    this.previousResults.push(this.currentResult);
+
+}
+subHandler(){
+    const firstN = parseInt(this.firstNumber);
+    const secondN = parseInt(this.secondNumber);
+
+        this.currentResult = 'Result of '+firstN+'-'+secondN+' is ' +(firstN-secondN);
+        this.previousResults.push(this.currentResult);
+
+}
+multiplyHandler(){
+    const firstN = parseInt(this.firstNumber);
+    const secondN = parseInt(this.secondNumber);
+
+    this.currentResult = 'Result of '+firstN+'x'+secondN+' is ' +(firstN*secondN);
+    this.previousResults.push(this.currentResult);
+
+}
+divisionHandler(){
+    const firstN = parseInt(this.firstNumber);
+    const secondN = parseInt(this.secondNumber);
+
+    this.currentResult = 'Result of '+firstN+'/'+secondN+' is ' +(firstN/secondN);
+    this.previousResults.push(this.currentResult);
+
+}
+showPreviousResultToggle(event){
+    this.showPreviousResults = event.target.checked;
+}
+}
